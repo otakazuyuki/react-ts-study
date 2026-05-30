02c4297d75e9d78a00b84b2124599fb38d1135e0
ota <ota.kazuyuki.s6@dc.tohoku.ac.jp>
2026-05-30 20:39:01 +0900

    ヘッダーとの連携機能を追加

--- src/NameForm.tsx
+++ src/NameForm.tsx
@@ -1,18 +1,19 @@
-import {useState} from "react";
+type NameFormProps={
+    userName:string;
+    onNameChange:(value:string)=>void;
+};
 
-export const NameForm=()=>{
-    const [text,setText]=useState<string>("");
-
+export const NameForm=({userName,onNameChange}:NameFormProps)=>{
     const handleChange=(e:any)=>{
-        setText(e.target.value);
+        onNameChange(e.target.value);
     };
 
     return(
         <div style={{display:"flex",flexDirection:"column",alignItems:"center",gap:"15px"}}>
             <h2>入力フォーム</h2>
-            <input type="text" value={text} onChange={handleChange} style={{padding:"10px",fontSize:"16px",width:"250px"}} placeholder="ここに入力"/>
+            <input type="text" value={userName} onChange={handleChange} style={{padding:"10px",fontSize:"16px",width:"250px"}} placeholder="ここに入力"/>
             <div style={{fontSize:"16px"}}>
-                <strong>入力された文字：</strong> {text}
+                <strong>入力された文字：</strong> {userName}
             </div>
         </div>
     );
